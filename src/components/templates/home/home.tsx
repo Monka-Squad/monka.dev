e2186b5f7bab9c30a34fab89b80f1846d340559c
Lachlan Underhill <lachlanu@gmail.com>
2022-11-12 11:55:30 +1000

    fix: mobile stying
    *twerks furiously*

--- src/components/templates/home/home.tsx
+++ src/components/templates/home/home.tsx
@@ -4,10 +4,12 @@ import Monke from "./monke";
 const Home = () => {
 	return (
 		<main className="h-screen flex justify-center items-center duration-200 select-none">
-			<div className="flex flex-col items-center gap-10">
-				<h1 className="text-6xl font-display font-bold">Monka Squad</h1>
+			<div className="flex flex-col items-center p-5 gap-16">
+				<h1 className="text-6xl font-display font-bold text-center">
+					Monka Squad
+				</h1>
 
-				<p className="text-lg font-mono">
+				<p className="sm:text-lg font-mono text-center">
 					haha funny <Monke />
 				</p>
 			</div>
